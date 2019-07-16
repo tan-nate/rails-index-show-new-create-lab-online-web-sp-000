54eb43e28285e5faf31d05ab4a756dd4e67e4d60
Nate Tan <nktan93@gmail.com>
2019-07-16 16:19:45 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -54,5 +54,5 @@ Rails.application.routes.draw do
   #     resources :products
   #   end
 
-  resources :students, only: [:index, :show, :new, :create]
+  resources :coupons, only: [:index, :show, :new, :create]
 end
